9936e9f40b0d7dcc14daf5b1779d187cd11e2e72
chen <chen.bys@foxmail.com>
2018-01-21 21:19:10 +0800

    added smooth_l1 loss and solved about train_iter

--- sample.py
+++ sample.py
@@ -43,3 +43,16 @@ def get_samples(label_feat, pos_number=200, neg_number=200):
 
     return np.vstack((pos_samples[pos_select_index], neg_samples[neg_select_index])), \
            np.hstack((np.ones((pos_number,)), np.zeros((neg_number,))))
+
+
+def get_samples_with_iou_label(label_feat, pos_number=3000, neg_number=5000):
+    import mxnet as mx
+    x, y, w, h = label_feat[0, :]
+    feat_bboxes = sample_on_feat(1, 1, 1, 1, w, h)
+    feat_bboxes_ = util.x1y2x2y22xywh(feat_bboxes[:, 1:5])
+    rat = util.overlap_ratio(label_feat, feat_bboxes_)
+    pos_idx = mx.ndarray.topk(mx.ndarray.array(rat), axis=0, k=pos_number).asnumpy().astype('int32')
+    neg_idx = mx.ndarray.topk(mx.ndarray.array(rat * -1), axis=0, k=neg_number).asnumpy().astype('int32')
+    all_idx=np.hstack((pos_idx, neg_idx))
+
+    return feat_bboxes[all_idx,:], rat[all_idx]
