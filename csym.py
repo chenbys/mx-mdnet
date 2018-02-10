6b0ee2ead686c8eef76b29c08f720a01943b5485
chen <chen.bys@foxmail.com>
2018-02-10 15:17:28 +0800

    c

--- csym.py
+++ csym.py
@@ -186,9 +186,7 @@ def get_mdnet_with_CE_loss_s(prefix=''):
     score = mx.symbol.Convolution(data=drop5, kernel=(1, 1), stride=(1, 1), num_filter=1, name=prefix + 'score')
     score_ = mx.symbol.Reshape(data=score, shape=(-1), name=prefix + 'score_re')
     # pos_pred: (K,)
-    pos_pred = mx.symbol.slice(score_, begin=(None, 0), end=(None, 1), name='sliced_pos_pred').reshape((-1,),
-                                                                                                       name=prefix + 'pos_pred')
-    smooth_l1 = mx.symbol.smooth_l1(data=pos_pred - label_, scalar=1, name=prefix + 'smooth_l1')
+    smooth_l1 = mx.symbol.smooth_l1(data=score_ - label_, scalar=1, name=prefix + 'smooth_l1')
     loss = mx.symbol.MakeLoss(data=smooth_l1, normalization='null', grad_scale=1, name='loss')
 
     # return loss, conv1, lrn2
