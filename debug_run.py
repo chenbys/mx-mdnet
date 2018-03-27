c3a70b186755bdf78523bdc71633e9f1ff7c934e
Junjie Chen <chen.bys@outlook.com>
2018-03-27 14:31:50 +0800

    multi-track does work better, but slow down model

--- debug_run.py
+++ debug_run.py
@@ -15,6 +15,7 @@ from setting import const
 import matplotlib.pyplot as plt
 from matplotlib import patches
 import logging
+import kit
 
 update_data_queue = Queue.Queue(maxsize=100)
 
@@ -62,7 +63,7 @@ def debug_track_seq(args, model, img_paths, gts):
         img = plt.imread(img_paths[cur])
         T = time.time()
         # track
-        region, prob = track(model, img, pre_region=region, gt=gts[cur])
+        region, prob = multi_track(model, img, pre_region=region, gt=gts[cur])
 
         res.append(region)
         probs.append(prob)
@@ -104,6 +105,10 @@ def debug_track_seq(args, model, img_paths, gts):
     return res, probs, ious
 
 
+def check_track(model, i, plotc=False, showc=False, checkc=False):
+    track(model, plt.imread(const.img_paths[i]), const.gts[i], const.gts[i], plotc=plotc, showc=showc, checkc=checkc)
+
+
 def get_update_data(frame_len=20):
     '''
         返回最近frame_len帧 组成的 update_data
@@ -113,7 +118,7 @@ def get_update_data(frame_len=20):
     frame_len = min(frame_len, update_data_queue.qsize())
     img_patches, feat_bboxes, labels = [], [], []
 
-    for i in range(1, frame_len + 5):
+    for i in range(1, frame_len):
         a, b, c = update_data_queue.queue[-(i % frame_len)]
         img_patches += a
         feat_bboxes += b
@@ -158,24 +163,59 @@ def online_update(args, model, data_len=20):
     :param num_epoch:
     :return:
     '''
-    T = time.time()
     update_iter = datahelper.get_iter(get_update_data(data_len))
-    T2 = time.time()
 
     model.fit(train_data=update_iter, optimizer='sgd',
               eval_metric=mx.metric.CompositeEvalMetric(
-                  [extend.PR(0.5), extend.RR(0.5), extend.TrackTopKACC(10, 0.6)]),
+                  [extend.SMLoss(), extend.PR(0.5), extend.RR(0.5), extend.TrackTopKACC(10, 0.6)]),
               optimizer_params={'learning_rate': args.lr_offline,
                                 'wd': args.wd,
                                 'momentum': args.momentum,
                                 # 'clip_gradient': 5,
                                 },
               begin_epoch=0, num_epoch=args.num_epoch_for_online)
-    # logging.getLogger().info('@CHEN->get update iter:%.4f,fit:%.4f' % (T2 - T, time.time() - T2))
     return model
 
 
-def track(model, img, pre_region, gt):
+def multi_track(model, img, pre_region, gt, topK=5):
+    pre_regions = []
+    for ws in [0.5, 1, 1.5, 2]:
+        for hs in [0.5, 1, 1.5, 2]:
+            pre_regions.append(util.central_bbox(pre_region, ws, hs))
+
+    A, B = [], []
+    for pr in pre_regions:
+        t = time.time()
+        bboxes, probs = track(model, img, pr, gt, topK=topK)
+        A.append(bboxes)
+        B.append(probs)
+        # print 'time for track:%.5f' % (time.time() - t)
+    idx = np.array(B).reshape(-1, ).argsort()[-16::]
+    x_y_idx = [divmod(i, topK) for i in idx]
+    top_bboxes = []
+    top_probs = []
+    for x, y in x_y_idx:
+        top_bboxes.append(A[x][y, :])
+        top_probs.append(B[x][y])
+    opt_img_bbox = np.mean(top_bboxes, 0)
+    opt_score = np.mean(top_probs)
+
+    def show_tracking():
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(img)
+        ax.add_patch(patches.Rectangle((opt_img_bbox[0], opt_img_bbox[1]), opt_img_bbox[2], opt_img_bbox[3],
+                                       linewidth=4, edgecolor='red', facecolor='none'))
+        ax.add_patch(patches.Rectangle((gt[0], gt[1]), gt[2], gt[3],
+                                       linewidth=1, edgecolor='blue', facecolor='none'))
+        ax.add_patch(patches.Rectangle((pre_region[0], pre_region[1]), pre_region[2], pre_region[3],
+                                       linewidth=1, edgecolor='yellow', facecolor='none'))
+        fig.show()
+
+    return opt_img_bbox, opt_score
+
+
+def track(model, img, pre_region, gt, topK=5, plotc=False, showc=False, checkc=False):
     pred_data, restore_info = datahelper.get_predict_data(img, pre_region)
     pred_iter = datahelper.get_iter(pred_data)
     [img_patch], [feat_bboxes], [l] = pred_data
@@ -185,8 +225,6 @@ def track(model, img, pre_region, gt):
     patch_bboxes = util.feat2img(feat_bboxes[:, 1:])
     img_bboxes = util.restore_img_bbox(patch_bboxes, restore_info)
     labels = util.overlap_ratio(gt, img_bboxes)
-    # 按照输出概率的最大topK个的bbox来平均出结果
-    topK = 5
     top_idx = pos_score.argsort()[-topK::]
     top_scores = pos_score[top_idx]
     top_feat_bboxes = feat_bboxes[top_idx, 1:]
@@ -226,6 +264,7 @@ def track(model, img, pre_region, gt):
         ax.add_patch(patches.Rectangle((pre_region[0], pre_region[1]), pre_region[2], pre_region[3],
                                        linewidth=1, edgecolor='yellow', facecolor='none'))
         fig.show()
+        return top_img_bboxes, top_scores
 
     def check_all_metric(th=const.train_pos_th):
         # PR RR
@@ -279,7 +318,15 @@ def track(model, img, pre_region, gt):
     # check_all_metric(0.5)
     # check_all_metric(0.7)
     # check_train_metric()
-    return opt_img_bbox, opt_score
+    if plotc:
+        plot()
+    if showc:
+        show_tracking()
+    if checkc:
+        check_train_metric()
+        check_all_metric()
+
+    return top_img_bboxes, top_scores
 
 
 # track(model,plt.imread(const.img_paths[0]),const.gts[0],const.gts[0])
@@ -291,7 +338,7 @@ def debug_seq():
     vot = datahelper.VOTHelper(args.VOT_path)
     img_paths, gts = vot.get_seq('bolt2')
 
-    first_idx = 50
+    first_idx = 50  # f_idx=50 in bolt2, frame 189开始bbox变大, 虽然它自己不知道（probs>0.6），但可以通过long term update扭转回来
     img_paths, gts = img_paths[first_idx:], gts[first_idx:]
 
     # for debug and check
@@ -317,11 +364,11 @@ def parse_args():
     parser.add_argument('--VOT_path', help='VOT folder', default='/media/chen/datasets/VOT2015', type=str)
     parser.add_argument('--ROOT_path', help='cmd folder', default='/home/chen/mx-mdnet', type=str)
 
-    parser.add_argument('--lr_step', default=307 * 1, help='every x num for y epoch', type=int)
+    parser.add_argument('--lr_step', default=307 * 2, help='every x num for y epoch', type=int)
     parser.add_argument('--lr_factor', default=0.8, help='20 times will be around 0.1', type=float)
-    parser.add_argument('--lr_stop', default=2e-5, type=float)
+    parser.add_argument('--lr_stop', default=1e-5, type=float)
 
-    parser.add_argument('--wd', default=0, help='weight decay', type=float)
+    parser.add_argument('--wd', default=5e0, help='weight decay', type=float)
     parser.add_argument('--momentum', default=0.9, type=float)
     parser.add_argument('--lr_offline', default=2e-5, help='base learning rate', type=float)
     parser.add_argument('--lr_online', default=1e-5, help='base learning rate', type=float)
--- kit.py
+++ kit.py
@@ -1,7 +1,16 @@
-# from setting import config
-#
-#
-# def p(msg, level=0, prefix='@Chen ->', ):
-#     if level >= config.p_level:
-#         print prefix + str(msg)
-#
+import matplotlib.pyplot as plt
+from matplotlib import patches
+import numpy as np
+
+
+def show_tracking(img, bboxes):
+    bboxes = np.array(bboxes)
+    if bboxes.ndim == 1:
+        bboxes = np.array([bboxes])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(img)
+    for i in range(bboxes.shape[0]):
+        ax.add_patch(patches.Rectangle((bboxes[i, :][0], bboxes[i, :][1]), bboxes[i, :][2], bboxes[i, :][3],
+                                       linewidth=2, edgecolor='red', facecolor='none'))
+    fig.show()
